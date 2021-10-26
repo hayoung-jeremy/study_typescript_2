df929df07565d10b9d23aed2f1b0bf5f18af776b
Ha young, Kim <jeremiah91@naver.com>
2021-10-26 15:28:54 +0900

    feat : types, interface, class
     - it prevents you from making human mistakes in programming

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, and you are ${gender}`;
+// have to define the properties of the Class, and the permissions of them
+class Human {
+  public name: string;
+  public age: number;
+  // private age: number;
+  // use private property to protect the info, cannot access private data out side the class
+  // ex) cannot use [person.age] in sayHi()
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+// interface :
+
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+
+const hayoung = new Human("Ha young", 31, "male");
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, and you are ${person.gender}`;
 };
 
-console.log(sayHi("Ha young", 31, "male"));
+console.log(sayHi(hayoung));
 
 export {};
