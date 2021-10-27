acf737e6550f4e98182f06ae0cbb28c668ce3d5a
Ha young, Kim <jeremiah91@naver.com>
2021-10-27 13:46:14 +0900

    feat : Create Block sample-1

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,35 @@
-// have to define the properties of the Class, and the permissions of them
-class Human {
-  public name: string;
-  public age: number;
-  // private age: number;
-  // use private property to protect the info, cannot access private data out side the class
-  // ex) cannot use [person.age] in sayHi()
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
+const genesisBlock: Block = new Block(
+  0,
+  "asdasfas222",
+  "",
+  "hello data",
+  123456
+);
 
-// interface :
+// blockchain 에 추가될 데이터는 block 으로만 한정시킬 수 있음
+let blockchain: [Block] = [genesisBlock];
 
-// interface Human {
-//   name: string;
-//   age: number;
-//   gender: string;
-// }
-
-const hayoung = new Human("Ha young", 31, "male");
-
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, and you are ${person.gender}`;
-};
-
-console.log(sayHi(hayoung));
+console.log(blockchain);
 
 export {};
