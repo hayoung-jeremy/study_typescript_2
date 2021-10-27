fde0aa6b052e43c74e977f601c70794674548b2e
Ha young, Kim <jeremiah91@naver.com>
2021-10-27 14:06:14 +0900

    feat : Create Block sample-2
     - install library : crypto-js
     - hash function and static method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  // static method: can be used without creating an instance of the Class
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
   constructor(
     index: number,
@@ -28,8 +39,13 @@ const genesisBlock: Block = new Block(
 );
 
 // blockchain 에 추가될 데이터는 block 으로만 한정시킬 수 있음
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
+
+// latest item of the chain
+const getLatestBlcok = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
